80b8511b5442f6ece25ba741e795c348a46f9ef2
Urchin Colley <urchin@simspace.com>
2020-11-24 16:55:12 -0800

    Add test data for events and players

---
+++ db/migrations/20201124084135_populate_events.up.sql
@@ -0,0 +1,8 @@
+INSERT INTO public.events (name) VALUES
+('Pro Tour London'),
+('Grand Prix Detroit'),
+('US Nationals'),
+('World Championships'),
+('SCG Championships'),
+('SCG Regionals'),
+('Guardian Games FNM')
